85476ed462f9d351a23604f1c4c21c3318ac28de
Admin-Cataldi <admin-bcataldi@neit.edu>
2017-10-27 11:45:59 -0400

    Initializing Assignment 4

--- assignments/2/demo/routes/form.js
+++ assignments/2/demo/routes/form.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
     var msg = '';
     if (req.query.empty === 'true') {
