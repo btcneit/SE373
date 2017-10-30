aa1d828a21e4c455d261e019ba9c3f3cbb34a27d
Admin-Cataldi <admin-bcataldi@neit.edu>
2017-10-30 16:07:45 -0400

    Working on Assignment 4, and fixed a weird issue with assignment 2

--- assignments/4/demo/app.js
+++ assignments/4/demo/app.js
@@ -4,11 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 require('./db');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -21,11 +21,11 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator()); 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- assignments/4/demo/routes/index.js
+++ assignments/4/demo/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var ctrlEmployee = require('../controllers/employee.controller.js');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.all('/', ctrlEmployee.home);
+router.all('/view', ctrlEmployee.view);
+router.all('/update/:id?', ctrlEmployee.update);
+router.all('/delete/:id?', ctrlEmployee.delete);
 
 module.exports = router;
